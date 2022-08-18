37ef1fc2b9b05d5507fc7ee38cdd9e302cfe61d1
dootopia <syo.dootopia@gmail.com>
2022-08-18 16:04:23 +0900

    first

---
+++ src/product/product.controller.ts
@@ -0,0 +1,58 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Req } from '@nestjs/common';
+import { get } from 'http';
+import { Product } from './entities/product.entity';
+import { ProductService } from './product.service';
+
+@Controller('product')
+export class ProductController {
+  
+  constructor(private readonly productService: ProductService){}
+
+  @Get()
+  getList(): Product[] {
+    // return "get list";
+    return this.productService.getList();
+  }
+
+  // @Get("test")
+  // findAll(@Req() request: Request): string{
+  //   return `Requests : ${Request} `;
+  // }
+
+  @Get("search")
+  search(@Query("year") searchYear: string, searchDate: string){
+    return `search yaer : ${searchYear} , search date : ${searchDate}`;
+  }
+
+  @Get('/:id')
+  getProduct(@Param("id") productId: string) : Product{
+    // return `get Product required id : ${productId}`;
+    return this.productService.getProduct(productId);
+  }
+
+  @Post()
+  create(@Body() productData){
+    // console.log("productData : ", productData)
+    // return `create api`;
+    // return productData;
+    return this.productService.create(productData);
+  }
+
+  @Delete('/:id')
+  remove(@Param("id") productId: string){
+    // return `remove required id : ${productId}`;
+    return this.productService.deleteOne(productId);
+  }
+
+  @Patch('/:id')
+  patch(@Param("id") productId: string, @Body() updateData){
+    
+    // return `remove required id : ${productId}`;
+    return {
+      updatedProduct: productId,
+      ...updateData
+    }
+  }
+
+
+}
---
+++ src/product/product.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductService {
+
+  private products: Product[] = [];
+
+  getList(): Product[]{
+    return this.products;
+  }
+
+  getProduct(id:string): Product {
+
+    // parseInt(id) === +id 같은 의미
+    // console.log("이건 뭔 product : ", product)
+    // return 1;
+    
+    const product = this.products.find(product => product.id === +id)
+
+    if(!product){
+      throw new NotFoundException(`product with id ${id} not found`);
+    }
+
+    return product;
+  }
+
+  deleteOne(id:string):boolean {
+    this.products.filter(product => product.id !== +id)
+    return true;
+  }
+
+  create(productData){
+    this.products.push({
+        id: this.products.length + 1,
+        ...productData,
+    })
+  }
+
+}
